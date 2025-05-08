ac3f0adf782fc96ec5d50c956bb6866b13d217a0
강일빈 <kib3388@naver.com>
2025-05-08 21:07:10 +0900

    파일 구조 변경중...

--- app/(account)/_layout.tsx
+++ app/(account)/_layout.tsx
@@ -1,6 +1,4 @@
 import { Tabs } from 'expo-router';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 export default function TabLayout() {
     return (
         <Tabs>
@@ -9,10 +7,6 @@ export default function TabLayout() {
                 options={{
                     title: '계좌',
                 }}
-            />
-            <Tabs.Screen
-                name="settings"
-                options={{ title: '설정' }}
             />
         </Tabs>
     );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,31 @@ import {Stack, useRouter} from 'expo-router';
 import {useEffect} from "react";
 import * as SecureStore from "expo-secure-store";
 import {refreshAccessToken} from "../contexts/backEndApi";
-import { Stack, useRouter } from 'expo-router';
+import {Alert} from "react-native";
 
 export default function StackLayout() {
+    const router = useRouter();
+
+    useEffect(() => {
+        (async () => {
+            const token = await SecureStore.getItemAsync('refresh_token');
+            if (token) {
+                try {
+                    const response = await refreshAccessToken(token);
+                    return router.replace(response ? '/account' : '/login');
+                } catch {
+                    console.error('Error refreshing token');
+                }
+            }
+            Alert.alert('로그인 필요', '로그인 화면으로 이동합니다.');
+            router.replace('/login');
+        })();
+    }, []);
 
     return (
         <Stack>
-            <Stack.Screen name="auth/login" options={{ headerShown: false }} />
-            <Stack.Screen name="account/index" options={{ headerShown: false }} />
+            <Stack.Screen name="/login" options={{ headerShown: false }} />
+            <Stack.Screen name="/account" options={{ headerShown: false }} />
         </Stack>
     );
 }
