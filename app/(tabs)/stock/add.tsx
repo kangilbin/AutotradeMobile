a81a525ce791ecb83f50a99cae68297c4fd71c63
강일빈 <kib3388@naver.com>
2025-07-17 22:43:04 +0900

    auto 등록 버튼

---
+++ app/(tabs)/stock/add.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+
+
+export default function AddStockScreen() {
+    return (
+        <View>
+            <Text>Add Stock</Text>
+        </View>
+    );
+}
